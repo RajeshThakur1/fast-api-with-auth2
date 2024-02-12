51ee26558be04bd10db9d22aa023268e31298f48
Rajesh123222 <Rajesh.Kumar3@synechron.com>
2024-02-12 19:24:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import models
+from database import engine, SessionLocal
+from routers import auth, todos, admin, users
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+# app.include_router(admin.router)
+# app.include_router(users.router)
+        
